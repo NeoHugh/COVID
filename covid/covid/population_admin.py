0669e750854ec968c6ec30271f729bca8dd46023
Llux-C <65882191+Llux-C@users.noreply.github.com>
2020-06-17 19:08:24 +0800

    Update population_admin.py

--- covid/covid/population_admin.py
+++ covid/covid/population_admin.py
@@ -3,7 +3,7 @@ from flask import request,redirect
 from covid import db
 from covid.models import Users, Admin,Information,Comment,Popflow
 listInfoType = [
-    '复工复产','社区举措','交通出行','医疗举措'
+    '复工复产','社区举措','医疗举措','交通出行'
 ]
 
 listInfoProvince = [
